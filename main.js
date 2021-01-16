0cfbaba6c938404033823cb9fa01f4d7a96997eb
Maxim Pypko <max-rewq@rambler.ru>
2021-01-16 20:54:12 +0200

    home work 19

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-//поместил список из пяти li-шек на страницу
 const $Ul = document.createElement('ul');
 
-for (let i = 0; i < 5; i++) {
-  const $Li= document.createElement('li');
+for (let i = 1; i <= 5; i++) {
+  const $Li = document.createElement('li');
+  $Li.innerText = i;
   $Ul.append($Li);
 }
 document.body.append($Ul);
 
-//на список навешиваем событие
+
 $Ul.addEventListener('click', function (e) {
   
-  if (e.target.tagName === 'LI') { //событие будет работать если кликнули по li
-    e.target.style.order += 10     
+  if (e.target.tagName === 'LI') {
+    this.appendChild(e.target);
 
-    if (!e.target.style.background) { //если у li нет фона, значит задать этот фон
+    if (!e.target.style.background) { 
       e.target.style.background = 'blue'; 
-      
-    } else if (e.target.style.background == 'blue') { //если у li во такой фон, значит 
-      e.target.style.background = 'green';            //задать этот фон и т.д.
+     
+    } else if (e.target.style.background == 'blue') { 
+      e.target.style.background = 'green';
       
     } else if (e.target.style.background == 'green') {
       e.target.style.background = 'yellow';
-  
+      
     } else if (e.target.style.background == 'yellow') {
       e.target.style.background = 'blue'; 
     }
   }
 })
+///////////////////////////////////////////
+
+function toggleClass(el, elementClass) { 
+  const stringElementClass = elementClass.trim();
+  const arrayOfClasses = el.className.split(' ');
+  const indexClass = arrayOfClasses.indexOf(stringElementClass);
+
+  indexClass < 0
+  ? arrayOfClasses.push(stringElementClass)
+  : arrayOfClasses.splice(indexClass, 1);
+
+  el.className = arrayOfClasses.join(' ');
+}
+
+const $div = document.createElement('div');
+$div.className = 'box shadow radius';
+
+toggleClass($div, 'border  ');
